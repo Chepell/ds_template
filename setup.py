3f6ce917553100672c618c5f9a402ce8d72d63e0
Artem Voytenko <av.voytenko@gmail.com>
2023-08-07 19:44:36 +0300

    Init

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='template for DS-projects base on cookiecutter',
+    author='Artem Voytenko',
+    license='',
+)
